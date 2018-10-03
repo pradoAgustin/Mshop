2872da641850cfbfb01fe5105bac43e181925b25
Agustin Prado <agustin.prado@despegar.com>
2018-10-03 16:24:18 -0300

    modified productListResponse

--- app/src/main/java/com/agustin/mlshop/domain/MainActivityListProductsResponse.kt
+++ app/src/main/java/com/agustin/mlshop/domain/MainActivityListProductsResponse.kt
@@ -1,5 +1,6 @@
 package com.agustin.mlshop.domain
 
+import com.agustin.mlshop.utils.LogError
 import com.google.gson.annotations.SerializedName
 
 data class MainActivityListProductsResponse(
@@ -11,6 +12,18 @@ data class MainActivityListProductsResponse(
         val currencies: List<ProductCurrency>?
 ) {
     fun filterCategoriesByName(): Map<String, String>? {
-        return categories?.map { it.name to it.id }?.toMap()
+        if (categories != null) {
+            val map = HashMap<String, String>()
+            for (category in categories) {
+                if (category.name != null && category.id != null) {
+                    map[category.name] = category.id
+                } else {
+                    LogError("category from service with bad parameters")
+                }
+
+            }
+            return map
+        }
+        return null
     }
 }
